1a17f0238de3b7ac76531fa867df02bd9916bed8
Manpreet <manpreet02701@gmail.com>
2024-03-20 12:17:11 +0530

    Added the code of templete.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-static footer-light navbar-border">
+      <p className="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2">
+        <span className="float-md-left d-block d-md-inline-block">
+          Copyright &copy; 2020{" "}
+          <a
+            className="text-bold-800 grey darken-2"
+            href="https://1.envato.market/pixinvent_portfolio"
+            target="_blank"
+          >
+            PIXINVENT{" "}
+          </a>
+        </span>
+        <span className="float-md-right d-none d-lg-block">
+          Hand-crafted & Made with <i className="feather icon-heart pink"></i>
+        </span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import "./app-assets/vendors/css/vendors.min.css";
+// import "./app-assets/css/bootstrap.css";
+// import "./app-assets/css/bootstrap-extended.css";
+// import "./app-assets/css/colors.css";
+// import "./app-assets/css/components.css";
+// import "./app-assets/css/core/menu/menu-types/vertical-menu-modern.css";
+// import "./app-assets/css/core/colors/palette-gradient.css";
+// import "./app-assets/fonts/simple-line-icons/style.min.css";
+// import "./app-assets/css/pages/card-statistics.css";
+// import "./app-assets/css/pages/vertical-timeline.css";
+// import "./assets/css/style.css";
+// import "./app-assets/vendors/js/vendors.min.js";
+// import "./app-assets/vendors/js/charts/apexcharts/apexcharts.min.js";
+// import "./app-assets/js/core/app-menu.js";
+// import "./app-assets/js/core/app.js";
+// import "./app-assets/js/scripts/cards/card-statistics.js";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
