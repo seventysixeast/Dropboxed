8345e4d10282885b4e55a788a9838b405b2d62ff
76East <info@76east.com>
2024-03-20 17:43:08 +0530

    notification lists

--- frontend/src/routers/AdminRouter.jsx
+++ frontend/src/routers/AdminRouter.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Navigate, useRoutes } from "react-router-dom";
 import { Dashboard } from "../pages/Dashboard";
+import { NotificationComponent } from "../pages/NotificationComponent";
+import { BookingListComponent } from "../pages/BookingListComponent";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const AdminRouter = ({ logoutUser = null }) => {
   const route = useRoutes([
     { path: "/dashBoard", element: <Dashboard /> },
+    { path: "/notifications", element: <NotificationComponent /> },
+    { path: "/booking-list", element: <BookingListComponent /> },
     { path: "/*", element: <Navigate to="/dashboard" /> },
   ]);
   return (
