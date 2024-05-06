dfeb718a9c9a566721bba3b8a0f0e8d8bbf3c247
Ravi Kumar <kumarravi32832@gmail.com>
2024-05-06 16:36:10 +0530

    updates and merges

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,9 @@
 import React, { useEffect } from "react";
-import { toast } from 'react-toastify';
 import IndexRouter from "./routers/Index";
 import { AuthProvider } from "./context/authContext";
-import { decryptToken } from "./helpers/tokenUtils";
-import { verifyToken } from "./api/authApis";
 
 const App = () => {
-    useEffect(() => {
-        const getTokenFromUrl = () => {
-            const urlParams = new URLSearchParams(window.location.search);
-            return urlParams.get('token');
-        };
-
-        const handleTokenVerification = async (token) => {
-            try {
-                const decryptedToken = decryptToken(token);
-                const { success, accessToken, user, message } = await verifyToken(decryptedToken);
-                console.log("success", success)
-                if (success) {
-                    // Save user data and access token in localStorage
-                    localStorage.setItem('accessToken', accessToken);
-                    localStorage.setItem('isAuth', true);
-                    localStorage.setItem('user', JSON.stringify(user));
-                    
-                    // Show success toast
-                    // toast.success('Token verification successful');
-                    
-                    // Redirect to dashboard
-                    window.location.href = '/dashboard';
-                } else {
-                    // Show error toast
-                    toast.error(`Token verification failed: ${message}`);
-                    // Redirect to login page
-                    window.location.href = '/login';
-                }
-            } catch (error) {
-                console.error("Token verification failed:", error.message);
-                // Show error toast
-                toast.error('Token verification failed');
-                // Redirect to login page
-                window.location.href = '/login';
-            }
-        };
-
-        const token = getTokenFromUrl();
-
-        if (token) {
-            handleTokenVerification(token);
-        }
-
-    }, []);
+    
 
     return (
         <AuthProvider>
