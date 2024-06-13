700ee9ad5da27cae63826e58c980f32c4d8b5b91
76East <info@76east.com>
2024-06-13 17:42:45 +0530

    chnages

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -103,8 +103,8 @@ exports.login = async (req, res) => {
     if (isFirstLogin) {
       // Send Welcome email
       if (user.role_id === 3) {
-        var SEND_EMAIL = WELCOME_CLIENT_EMAIL(user.subdomain.charAt(0).toUpperCase() + user.subdomain.slice(1), user.email, user.logo, req.body.name, req.body.email, password);
-        sendEmail(user.email, `Welcome to ${user.subdomain.charAt(0).toUpperCase() + user.subdomain.slice(1)}!`, SEND_EMAIL);
+        var SEND_EMAIL = WELCOME_CLIENT_EMAIL(user_subdmain.charAt(0).toUpperCase() + user_subdmain.slice(1), user.email, user.logo, req.body.name, req.body.email, password);
+        sendEmail(user.email, `Welcome to ${user_subdmain.charAt(0).toUpperCase() + user_subdmain.slice(1)}!`, SEND_EMAIL);
       } else {
         var SEND_EMAIL = WELCOME_EMAIL();
         sendEmail(user.email, "Welcome to Studiio.au!", SEND_EMAIL);
