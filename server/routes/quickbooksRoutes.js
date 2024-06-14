471e681d468659791083aa24c59efdd0c2eca7d2
Ravi Kumar <kumarravi32832@gmail.com>
2024-06-14 13:35:18 +0530

    updates

--- server/routes/quickbooksRoutes.js
+++ server/routes/quickbooksRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const { authenticateToken } = require('../middleware/authMiddleware');
 
 router.get('/auth-url', authenticateToken, quickbooksController.getQuickBooksAuthUrl);
-router.get('/save-callback', quickbooksController.quickBooksCallback);
+router.get('/callback', quickbooksController.quickBooksCallback);
 
 module.exports = router;
